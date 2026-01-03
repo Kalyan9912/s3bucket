1cbca9a6981502d32454b7be143bb76caaf5a65a
root <root@ip-172-31-10-199.ap-south-1.compute.internal>
2026-01-03 09:47:36 +0000

    s3 commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws"{
+    region="ap-south-1"
+}
+
+resource "aws_s3_bucket" "mybkp"{
+  
+    bucket="kalyan9912bucket"
+    tags={
+        Name="dev"
+    }
+      object_lock_enabled = true
+}
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.mybkp.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+resource "aws_s3_bucket_versioning" "myversion"{
+    bucket=aws_s3_bucket.mybkp.id
+    versioning_configuration{
+        status="Enabled"
+    }
+}
+
